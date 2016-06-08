ca411816d5c4ac3d11e195632dd46f025f504c84
Jelle Vandebeeck <jelle@fousa.be>
2016-06-08 11:51:56 +0200

    Remove the run target from the example project.

---
+++ Example/Pods/Target Support Files/Duvel/Duvel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Duvel : NSObject
+@end
+@implementation PodsDummy_Duvel
+@end
---
+++ Example/Pods/Target Support Files/Pods-Duvel_Tests/Pods-Duvel_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Duvel_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Duvel_Tests
+@end
